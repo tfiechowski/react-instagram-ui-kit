9f870d05aa07b52afdb539deb398c1b4e0434af6
Tomasz Fiechowski <tomasz.fiechowski@gmail.com>
2019-07-30 17:11:33 +0700

    Make styleguide build deterministic

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,13 +1,18 @@
 module.exports = {
-    webpackConfig: {
-      module: {
-        rules: [
-          {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-          }
-        ]
-      }
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        }
+      ]
     }
+  },
+  dangerouslyUpdateWebpackConfig(webpackConfig) {
+    webpackConfig.output.filename = "build/bundle.js";
+    webpackConfig.output.chunkFilename = "build/[name].js";
+    return webpackConfig;
   }
+};
