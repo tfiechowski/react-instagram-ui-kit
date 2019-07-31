fe95444410f0a32e91a2aedbadf0b02d285cc3a3
Tomasz Fiechowski <tomasz.fiechowski@gmail.com>
2019-07-31 13:14:40 +0700

    Prevent Emotion from generating source maps (size)

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
-import external from "rollup-plugin-peer-deps-external";
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
+import external from "rollup-plugin-peer-deps-external";
 import replace from "rollup-plugin-replace";
-import autoExternal from "rollup-plugin-auto-external";
-import { uglify } from "rollup-plugin-uglify";
 import { terser } from "rollup-plugin-terser";
+import { uglify } from "rollup-plugin-uglify";
 
 const NODE_ENV = process.env.NODE_ENV || "development";
+
+const isProd = () => NODE_ENV === "production";
 
 const plugins = [
   replace({
@@ -23,27 +24,30 @@ const plugins = [
 
 const prodPlugins = [];
 
-if (NODE_ENV === "production") {
+if (isProd()) {
   plugins.push(...prodPlugins);
 }
+
+const outputFileExtension = isProd() ? "js" : "min.js";
+const sourcemap = !isProd();
 
 export default [
   {
     input: "./src/index.js",
     output: {
-      file: "lib/index.js",
+      file: `lib/index.${outputFileExtension}`,
       format: "cjs",
-      sourcemap: true
+      sourcemap
     },
-    plugins: [...plugins, uglify()]
+    plugins: [...plugins, ...(isProd() ? [uglify()] : [])]
   },
   {
     input: "./src/index.js",
     output: {
-      file: "lib/index.esm.js",
+      file: `lib/index.esm.${outputFileExtension}`,
       format: "esm",
-      sourcemap: true
+      sourcemap
     },
-    plugins: [...plugins, terser()]
+    plugins: [...plugins, ...(isProd() ? [terser()] : [])]
   }
 ];
