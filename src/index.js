c88a03efccd1b80daa23ee96aa259b27bc1185c1
Tomasz Fiechowski <tomasz.fiechowski@gmail.com>
2019-07-31 11:51:53 +0700

    Remove redundant import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import App from "components/App";
 import Grid, { GridControlBar, GridControlBarItem } from "components/Grid";
 import Photo from "components/Photo";
 import Profile from "components/Profile";
-import React, { Component } from "react";
 
 export { Grid, GridControlBar, GridControlBarItem, App, Photo, Profile };
 
